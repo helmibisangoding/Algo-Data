2337b42856e24af188f76a261b8c07f308d96fde
helmibisangoding <hellmyrifai@gmail.com>
2026-05-23 23:53:52 +0700

    FINALISASI

--- algo.cpp
+++ algo.cpp
@@ -7,12 +7,37 @@ using namespace std;
 
 int jumlahPS = 3;
 
-struct PlayStation { int kodePS; char jenisPS[10]; int stokTotal; int stokTersedia; int hargaPerJam; };
-struct Transaksi { int idRental; char namaPenyewa[50]; int kodePS; char jenisPS[10]; int lamaSewa; int hargaPerJam; int totalBayar; char status[15]; };
-struct NodeRental { Transaksi info; NodeRental *kiri; NodeRental *kanan; };
+struct PlayStation {
+    int kodePS;
+    char jenisPS[10];
+    int stokTotal;
+    int stokTersedia;
+    int hargaPerJam;
+};
+
+struct Transaksi {
+    int idRental;
+    char namaPenyewa[50];
+    int kodePS;
+    char jenisPS[10];
+    int lamaSewa;
+    int hargaPerJam;
+    int totalBayar;
+    char status[15];
+};
+
+struct NodeRental {
+    Transaksi info;
+    NodeRental *kiri;
+    NodeRental *kanan;
+};
 NodeRental *awalRental = NULL, *akhirRental = NULL;
 NodeRental *awalRiwayat = NULL, *akhirRiwayat = NULL;
-struct NodeAntrian { Transaksi info; NodeAntrian *next; };
+
+struct NodeAntrian {
+    Transaksi info;
+    NodeAntrian *next;
+};
 NodeAntrian *kepalaAntrian = NULL;
 
 PlayStation daftarPS[3];
@@ -20,12 +45,18 @@ int counterIDRental = 101;
 int counterIDAntrian = 501; 
 
 void inisialisasiSistem() {
-    daftarPS[0].kodePS = 1; strcpy(daftarPS[0].jenisPS, "PS3"); daftarPS[0].stokTotal = 2; daftarPS[0].stokTersedia = 2; daftarPS[0].hargaPerJam = 5000;
-    daftarPS[1].kodePS = 2; strcpy(daftarPS[1].jenisPS, "PS4"); daftarPS[1].stokTotal = 3; daftarPS[1].stokTersedia = 3; daftarPS[1].hargaPerJam = 8000;
-    daftarPS[2].kodePS = 3; strcpy(daftarPS[2].jenisPS, "PS5"); daftarPS[2].stokTotal = 2; daftarPS[2].stokTersedia = 2; daftarPS[2].hargaPerJam = 12000;
-    
+    daftarPS[0].kodePS = 1; strcpy(daftarPS[0].jenisPS, "PS3");
+    daftarPS[0].stokTotal = 2; daftarPS[0].stokTersedia = 2; daftarPS[0].hargaPerJam = 5000;
+
+    daftarPS[1].kodePS = 2; strcpy(daftarPS[1].jenisPS, "PS4");
+    daftarPS[1].stokTotal = 3; daftarPS[1].stokTersedia = 3; daftarPS[1].hargaPerJam = 8000;
+
+    daftarPS[2].kodePS = 3; strcpy(daftarPS[2].jenisPS, "PS5");
+    daftarPS[2].stokTotal = 2; daftarPS[2].stokTersedia = 2; daftarPS[2].hargaPerJam = 12000;
+
     kepalaAntrian = new NodeAntrian;
-    kepalaAntrian->info.idRental = -1; strcpy(kepalaAntrian->info.namaPenyewa, "DUMMY_HEAD");
+    kepalaAntrian->info.idRental = -1;
+    strcpy(kepalaAntrian->info.namaPenyewa, "DUMMY_HEAD");
     kepalaAntrian->next = kepalaAntrian; 
 }
 
@@ -37,7 +68,9 @@ void tampilkanKatalogPS() {
     cout << "-------------------------------------------------------\n";
     for (int i = 0; i < jumlahPS; i++) {
         cout << daftarPS[i].kodePS << "\t" << daftarPS[i].jenisPS << "\tRp " << daftarPS[i].hargaPerJam;
+        
         if (daftarPS[i].hargaPerJam >= 10000) cout << "\t"; else cout << "\t\t";
+        
         cout << daftarPS[i].stokTersedia << " Unit Tersedia (Dari " << daftarPS[i].stokTotal << ")\n";
     }
     cout << "=======================================================\n";
@@ -51,6 +84,7 @@ int cariIndeksPS(int kode) {
 void sisipAkhirRental(NodeRental **awal, NodeRental **akhir, Transaksi data) {
     NodeRental *NB = new NodeRental;
     NB->info = data; NB->kiri = NULL; NB->kanan = NULL;
+    
     if (*awal == NULL) { *awal = *akhir = NB; } 
     else { (*akhir)->kanan = NB; NB->kiri = *akhir; *akhir = NB; }
 }
@@ -121,6 +155,7 @@ void checkInSewa() {
         strcpy(t.status, "Aktif");
         sisipAkhirRental(&awalRental, &akhirRental, t);
         daftarPS[idx].stokTersedia--;
+        
         cout << "\n[Check In Berhasil] ID: " << t.idRental << " | Tagihan: Rp " << t.totalBayar << "\n";
     } else {
         t.idRental = counterIDAntrian++;
@@ -130,12 +165,14 @@ void checkInSewa() {
         NodeAntrian *bantu = kepalaAntrian;
         while (bantu->next != kepalaAntrian) bantu = bantu->next; 
         NB->next = kepalaAntrian; bantu->next = NB; 
+        
         cout << "\n[Sistem]: Room penuh. Anda masuk antrian dengan ID: " << t.idRental << "\n";
     }
 }
 
 void cetakRentalAktif() {
     if (awalRental == NULL) { cout << "[Sistem]: Tidak ada sesi aktif.\n"; return; }
+    
     cout << "\n--- DAFTAR SESI RENTAL PS AKTIF ---\n";
     NodeRental *bantu = awalRental;
     while (bantu != NULL) {
@@ -171,7 +208,7 @@ void checkOutSewa() {
     sisipAkhirRental(&awalRiwayat, &akhirRiwayat, dataSelesai);
 
     int idx = cariIndeksPS(hapus->info.kodePS);
-    if (idx != -1) daftarPS[idx].stokTersedia++;
+    if (idx != -1) daftarPS[idx].stokTersedia++; 
 
     lepasNodeRental(hapus); 
     delete hapus; 
@@ -181,11 +218,112 @@ void checkOutSewa() {
     cekAntrianOtomatis(dataSelesai.kodePS, idx); 
 }
 
+void urutkanRentalBerdasarkanTagihan() {
+    if (awalRental == NULL) { cout << "[Sistem]: Tidak ada sesi aktif untuk diurutkan.\n"; return; }
+    
+    bool ditukar; NodeRental *bantu;
+    do {
+        ditukar = false; bantu = awalRental;
+        while (bantu->kanan != NULL) {
+            if (bantu->info.totalBayar < bantu->kanan->info.totalBayar) {
+                Transaksi temp = bantu->info;
+                bantu->info = bantu->kanan->info;
+                bantu->kanan->info = temp;
+                ditukar = true;
+            }
+            bantu = bantu->kanan;
+        }
+    } while (ditukar);
+    cout << "[Sistem]: Sesi aktif berhasil dirapikan berdasarkan tagihan terbesar.\n";
+    cetakRentalAktif();
+}
+
+void simpanSemuaData() {
+    FILE *fRiwayat = fopen("riwayat.dat", "wb");
+    if (fRiwayat) {
+        NodeRental *b1 = awalRiwayat;
+        while (b1 != NULL) { fwrite(&(b1->info), sizeof(Transaksi), 1, fRiwayat); b1 = b1->kanan; }
+        fclose(fRiwayat);
+    }
+    FILE *fAktif = fopen("aktif.dat", "wb");
+    if (fAktif) {
+        NodeRental *b2 = awalRental;
+        while (b2 != NULL) { fwrite(&(b2->info), sizeof(Transaksi), 1, fAktif); b2 = b2->kanan; }
+        fclose(fAktif);
+    }
+    FILE *fAntrian = fopen("antrian.dat", "wb");
+    if (fAntrian) {
+        NodeAntrian *b3 = kepalaAntrian->next;
+        while (b3 != kepalaAntrian) { fwrite(&(b3->info), sizeof(Transaksi), 1, fAntrian); b3 = b3->next; }
+        fclose(fAntrian);
+    }
+    cout << "[Sistem]: Seluruh data berhasil dibackup ke Disk.\n";
+}
+
+void muatSemuaData() {
+    Transaksi data;
+    FILE *fRiwayat = fopen("riwayat.dat", "rb");
+    if (fRiwayat) {
+        while (fread(&data, sizeof(Transaksi), 1, fRiwayat) == 1) 
+            sisipAkhirRental(&awalRiwayat, &akhirRiwayat, data);
+        fclose(fRiwayat);
+    }
+    FILE *fAktif = fopen("aktif.dat", "rb");
+    if (fAktif) {
+        while (fread(&data, sizeof(Transaksi), 1, fAktif) == 1) {
+            sisipAkhirRental(&awalRental, &akhirRental, data);
+            
+            int idx = cariIndeksPS(data.kodePS);
+            if(idx != -1) daftarPS[idx].stokTersedia--; 
+            if(data.idRental >= counterIDRental) counterIDRental = data.idRental + 1;
+        }
+        fclose(fAktif);
+    }
+    FILE *fAntrian = fopen("antrian.dat", "rb");
+    if (fAntrian) {
+        while (fread(&data, sizeof(Transaksi), 1, fAntrian) == 1) {
+            NodeAntrian *NB = new NodeAntrian; NB->info = data;
+            NodeAntrian *bantu = kepalaAntrian;
+            while (bantu->next != kepalaAntrian) bantu = bantu->next;
+            NB->next = kepalaAntrian; bantu->next = NB;
+            if(data.idRental >= counterIDAntrian) counterIDAntrian = data.idRental + 1;
+        }
+        fclose(fAntrian);
+    }
+}
+
+void tampilkanLaporanRiwayat() {
+    FILE *f = fopen("riwayat.dat", "rb");
+    if (!f) { cout << "[Sistem]: Belum ada file laporan.\n"; return; }
+    
+    Transaksi data; int total = 0;
+    cout << "\n--- REKAP JURNAL RIWAYAT SEWA (DARI FILE) ---\n";
+    while (fread(&data, sizeof(Transaksi), 1, f) == 1) {
+        cout << "ID: " << data.idRental << " | " << data.namaPenyewa << " | " << data.jenisPS << " | Rp " << data.totalBayar << "\n";
+        total += data.totalBayar;
+    }
+    fclose(f); cout << "Total Akumulasi Pendapatan: Rp " << total << "\n";
+}
+
+void tampilkanDaftarTunggu() {
+    if (kepalaAntrian->next == kepalaAntrian) { cout << "[Sistem]: Daftar tunggu kosong.\n"; return; }
+    
+    cout << "\n--- DAFTAR ANTRIAN WAITING LIST ---\n";
+    NodeAntrian *bantu = kepalaAntrian->next;
+    while (bantu != kepalaAntrian) {
+        cout << "No. Tiket: " << bantu->info.idRental << " | Nama: " << bantu->info.namaPenyewa << " | Request: " << bantu->info.jenisPS << "\n";
+        bantu = bantu->next;
+    }
+}
+
 int main() {
-    inisialisasiSistem(); 
+    inisialisasiSistem();
+    muatSemuaData(); 
+    
     int pilihan;
     do {
         system("cls"); 
+
         cout << "\n==================================================\n";
         cout << "         SISTEM OPERASIONAL RENTAL PS             \n";
         cout << "==================================================\n";
@@ -198,25 +336,35 @@ int main() {
         cout << "[7] Analisis Billing Tertinggi (Sorting Bubble)\n";
         cout << "[8] Backup Seluruh Status Transaksi ke Disk\n";
         cout << "[9] Muat Pembukuan Total Kas dari Disk\n";
-        cout << "[0] Matikan Sistem\n";
+        cout << "[0] Matikan Sistem (Auto-Save)\n";
         cout << "--------------------------------------------------\n";
         cout << "Pilih Menu Navigasi: "; cin >> pilihan;
         
         cout << "\n"; 
+
         switch (pilihan) {
-            case 1: tampilkanKatalogPS(); break; 
+            case 1: tampilkanKatalogPS(); break;
             case 2: checkInSewa(); break;
             case 3: cetakRentalAktif(); break;
             case 4: perpanjangDurasiSewa(); break;
             case 5: checkOutSewa(); break;
-            case 6: cout << "[Sistem]: Fitur Antrian masih dikembangkan.\n"; break;
-            case 7: cout << "[Sistem]: Fitur Sorting masih dikembangkan.\n"; break;
-            case 8: cout << "[Sistem]: Fitur Backup masih dikembangkan.\n"; break;
-            case 9: cout << "[Sistem]: Fitur Laporan masih dikembangkan.\n"; break;
-            case 0: cout << "[Sistem]: Menutup aplikasi. Terimakasih.\n"; break;
+            case 6: tampilkanDaftarTunggu(); break;
+            case 7: urutkanRentalBerdasarkanTagihan(); break;
+            case 8: simpanSemuaData(); break;
+            case 9: tampilkanLaporanRiwayat(); break;
+            case 0: 
+                simpanSemuaData(); 
+                cout << "[Sistem]: Menutup aplikasi. Terimakasih.\n"; 
+                break;
             default: cout << "[Sistem]: Kode tidak terdaftar.\n";
         }
-        if (pilihan != 0) { cout << "\n"; system("pause"); }
+
+        if (pilihan != 0) {
+            cout << "\n";
+            system("pause"); 
+        }
+
     } while (pilihan != 0);
+
     return 0;
 }
